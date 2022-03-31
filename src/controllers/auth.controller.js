90da4f67759a0eca29860a246606d8f607ea7ba9
Andry Pebrianto <andrypeb227@gmail.com>
2022-03-31 19:20:08 +0700

    Create generateJwtToken

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt');
 const authModel = require('../models/auth.model');
 const { success, failed } = require('../utils/createResponse');
+const jwtToken = require('../utils/generateJwtToken');
 
 module.exports = {
   register: async (req, res) => {
@@ -26,14 +27,17 @@ module.exports = {
       const { email, password } = req.body;
       const user = await authModel.login(email);
 
+      // jika user ditemukan
       if (user.rowCount > 0) {
         const match = await bcrypt.compare(password, user.rows[0].password);
+        // jika password benar
         if (match) {
+          const token = await jwtToken(user.rows[0]);
           success(res, {
             code: 201,
             payload: null,
             message: 'Login success',
-            token: '123',
+            token,
           });
           return;
         }
